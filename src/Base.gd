ad174e93c1a29fcdc159cb23a8c349034c2be0a1
Arttu_Lakkala <arttuje.lakkala@gmail.com>
2024-01-27 18:05:06 +0200

    Additional functionality

--- src/Base.gd
+++ src/Base.gd
@@ -1,9 +1,51 @@
 extends Node2D
-@export var anserws : Array[String]
-@export var herrings : Array[String]
-@export var text : String
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var decision 
-	
+@export_file var limeric_file
 
+var spot = preload("res://Text_Spot.tscn")
+var text_drag = preload("res://Dragable_Text.tscn")
+var verse = 0
+var solved  = -1
+var lines
+	
+func _ready():
+	solved = -1
+	verse = 0
+	var file = FileAccess.open(limeric_file, FileAccess.READ)
+	var limeric = file.get_as_text()
+	lines = limeric.split("\n")
+	for i in 5:
+		var line
+		line = find_child(("Line_" + str(i)))
+		line.text = lines[i]
+		if lines[i].contains("___"):
+			var new_spot = spot.instantiate()
+			#Create all spot Disabled by default
+			#Named iteratively
+			new_spot.position = line.position
+			add_child(new_spot)
+			new_spot.name = ("spot" +str(verse))
+			verse +=1
+			
+	_create_options()
+
+
+func _create_options ():
+	solved +=1
+	if solved == verse:
+		#Win
+		pass
+	else:
+		var options =lines[solved+5].split(",")
+		var test = ("spot" +str(solved))
+		var active_spot = get_node(test)
+		active_spot.get_node("CollisionShape2D").set_disabled(false)
+		active_spot.answer = options[0]
+		active_spot.active = true
+		var counter = 0
+		for i in options:
+			var new_text = text_drag.instantiate()
+			new_text._set_Text(i)
+			new_text._set_spot(Vector2(100*counter,300.0))
+			add_child(new_text)
+			counter +=1
+	
--- src/Dragable_Text.gd
+++ src/Dragable_Text.gd
@@ -4,9 +4,19 @@ var active = false
 var is_inside_spot = false
 var body_ref
 var mouse_offset: Vector2
+var option
+var start_position =Vector2 (0.0, 0.0)
+
+signal wrong_answer
+signal correct_answer
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+func _set_Text(source):
+	get_node("Content").text = source
+	option = source
+	
+func _set_spot(start):
+	start_position = start
+	position = start
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if active:
@@ -23,7 +33,10 @@ func _process(_delta):
 			if is_inside_spot:
 				var mover = get_tree().create_tween()
 				mover.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
-
+				_check_answer(body_ref.answer)
+			else:
+				var mover = get_tree().create_tween()
+				mover.tween_property(self, "global_position", start_position, 0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered():
 	if not global.is_draging:
@@ -49,3 +62,12 @@ func _on_area_2d_body_exited(body):
 	if(body.is_in_group('Text_Spots')):
 		is_inside_spot = false
 		body_ref = body
+
+func _check_answer(answer):
+	print_debug(answer)
+	if (answer == option):
+		correct_answer.emit()
+	else:
+		wrong_answer.emit()
+		
+		
