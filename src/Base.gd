4ee4cec44f7417e3ca98a72524b5d8786179d8ed
Arttu_Lakkala <arttuje.lakkala@gmail.com>
2024-01-27 13:15:46 +0200

    Adding variables

--- src/Base.gd
+++ src/Base.gd
@@ -1,6 +1,9 @@
 extends Node2D
-
-
+@export var anserws : Array[String]
+@export var herrings : Array[String]
+@export var text : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var decision 
+	
+
