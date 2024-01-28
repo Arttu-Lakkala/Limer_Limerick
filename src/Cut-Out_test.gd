71b913319b21b39ae7f3dbc160f0f11fcf50006a
Arttu_Lakkala <arttuje.lakkala@gmail.com>
2024-01-28 09:35:59 +0200

    added animation logic

--- src/Cut-Out_test.gd
+++ src/Cut-Out_test.gd
@@ -4,54 +4,44 @@ extends Node2D
 func _ready():
 	# These two would be visible while choosing the word - and the true would turn to false when the correct choice is made.
 	$IdleMan.visible = true
-	
-	#Here starts the first animation.
-	$FallingMan.visible = true
-	$TreeDog.visible = true
-	
-	get_node("FallingMan").play("Fall")
-	get_node("TreeDog").play("Run")
-	get_node("FallingManAnimation").play("Man-falls")
-	get_node("TreeDogAnimation").play("new_animation")
-	
-	#Here starts the second animation.
-	$Treebottom.visible = true
-	$RunningDog.visible = true
-	$FallingMan2.visible = true
-	
-	get_node("RunningDog").play("Running")
-	get_node("FallingMan2").play("Falling")
-	get_node("FallingMan2Animation").play("Exploding Tree")
-	get_node("FullDogAnimation").play("RunningDogFull")
-	
-	#Here is the third.
-	$Tree.visible = true
-	
-	get_node("Tree/FallingMan3").play("Fall3")
-	get_node("Tree/ClimbingDog").play("Climb")
-	get_node("ClimbingAnimation").play("ClimbingUp")
-	
-	#Here is the fourth one.
-	$IntactTree.visible = true
-	
-	get_node("IntactTree/SpringyMan").play("Spring")
-	get_node("IntactTree/SpringyDog").play("SpringDog")
-	get_node("SpringyManAnimation").play("Springy-Man")
-	get_node("TreeDogAnimation").play("Springy-Dog")
 
 
 func _play_animation():
 	$IdleMan.visible = false
 	match global.level:
 		0:
-			pass
+			$FallingMan.visible = true
+			$TreeDog.visible = true
+			get_node("FallingMan").play("Fall")
+			get_node("TreeDog").play("Run")
+			get_node("FallingManAnimation").play("Man-falls")
+			get_node("TreeDogAnimation").play("new_animation")
+			$FallingMan.visible = false
+			$TreeDog.visible = false
 		1:
-			pass
+			$Treebottom.visible = true
+			$RunningDog.visible = true
+			$FallingMan2.visible = true
+			get_node("RunningDog").play("Running")
+			get_node("FallingMan2").play("Falling")
+			get_node("FallingMan2Animation").play("Exploding Tree")
+			get_node("FullDogAnimation").play("RunningDogFull")
+			$Treebottom.visible = false
+			$RunningDog.visible = false
+			$FallingMan2.visible = false
 		2:
-			pass
+			$Tree.visible = true
+			get_node("Tree/FallingMan3").play("Fall3")
+			get_node("Tree/ClimbingDog").play("Climb")
+			get_node("ClimbingAnimation").play("ClimbingUp")
+			$Tree.visible = false
 		3:
-			pass
-			
+			$IntactTree.visible = true
+			get_node("IntactTree/SpringyMan").play("Spring")
+			get_node("IntactTree/SpringyDog").play("SpringDog")
+			get_node("SpringyManAnimation").play("Springy-Man")
+			get_node("TreeDogAnimation").play("Springy-Dog")
+			$IntactTree.visible = false
 			
 	$IdleMan.visible = true
 	
